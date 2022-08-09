e13701fb85fca5fb6bd1ab9964416097d536e766
Ulisses <ulissesmilitao@hotmail.com>
2022-08-08 23:57:39 -0300

    add cpf validation

--- WebMVC/Controllers/CadastroController.cs
+++ WebMVC/Controllers/CadastroController.cs
@@ -20,61 +20,9 @@ namespace WebMVC.Controllers
             return View(clientes);
         }
 
-        private void PreecherDropDownLists(string sexo = null, string estadoCivil = null)
-        {
-            List<SelectListItem> lstSexo = new List<SelectListItem>();
-
-            lstSexo.Add(new SelectListItem()
-            {
-                Text = "Masculino",
-                Value = "M",
-                Selected = sexo != null && sexo == "M" ? true : false
-            });
-            lstSexo.Add(new SelectListItem()
-            {
-                Text = "Feminino",
-                Value = "F",
-                Selected = sexo != null && sexo == "F" ? true : false
-            });
-
-            List<SelectListItem> lstEstadoCivil = new List<SelectListItem>();
-
-            lstEstadoCivil.Add(new SelectListItem()
-            {
-                Text = "Solteiro(a)",
-                Value = "Solteiro",
-                Selected = estadoCivil != null && estadoCivil == "Solteiro" ? true : false
-            });
-
-            lstEstadoCivil.Add(new SelectListItem()
-            {
-                Text = "Casado(a)",
-                Value = "Casado",
-                Selected = estadoCivil != null && estadoCivil == "Casado" ? true : false
-            });
-
-            lstEstadoCivil.Add(new SelectListItem()
-            {
-                Text = "Divorciado(a)",
-                Value = "Divorciado",
-                Selected = estadoCivil != null && estadoCivil == "Divorciado" ? true : false
-            });
-
-            ViewBag.Sexo = new MultiSelectList(lstSexo.ToList(), "Value", "Text");
-            ViewBag.EstadoCivil = new MultiSelectList(lstEstadoCivil.ToList(), "Value", "Text");
-        }
-
-
-        // GET: Cadastro/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
         // GET: Cadastro/Create
         public ActionResult Create()
         {
-
             PreecherDropDownLists();
 
             return View();
@@ -86,12 +34,21 @@ namespace WebMVC.Controllers
         {
             try
             {
-                await api.PostCliente(model, HttpMethod.Post);
+                if (ValidarCpf(model.Cpf))
+                {
+                    await api.PostCliente(model, HttpMethod.Post);
 
-                return RedirectToAction("Index");
+                    return RedirectToAction("Index");
+                }
+
+                ViewBag.JavaScriptFunction = string.Format("CpfInvalido();");
+                
+                PreecherDropDownLists();
+                return View();
             }
             catch
             {
+                PreecherDropDownLists();
                 return View();
             }
         }
@@ -146,6 +103,87 @@ namespace WebMVC.Controllers
             {
                 return View();
             }
+        }
+
+        private void PreecherDropDownLists(string sexo = null, string estadoCivil = null)
+        {
+            List<SelectListItem> lstSexo = new List<SelectListItem>();
+
+            lstSexo.Add(new SelectListItem()
+            {
+                Text = "Masculino",
+                Value = "M",
+                Selected = sexo != null && sexo == "M" ? true : false
+            });
+            lstSexo.Add(new SelectListItem()
+            {
+                Text = "Feminino",
+                Value = "F",
+                Selected = sexo != null && sexo == "F" ? true : false
+            });
+
+            List<SelectListItem> lstEstadoCivil = new List<SelectListItem>();
+
+            lstEstadoCivil.Add(new SelectListItem()
+            {
+                Text = "Solteiro(a)",
+                Value = "Solteiro",
+                Selected = estadoCivil != null && estadoCivil == "Solteiro" ? true : false
+            });
+
+            lstEstadoCivil.Add(new SelectListItem()
+            {
+                Text = "Casado(a)",
+                Value = "Casado",
+                Selected = estadoCivil != null && estadoCivil == "Casado" ? true : false
+            });
+
+            lstEstadoCivil.Add(new SelectListItem()
+            {
+                Text = "Divorciado(a)",
+                Value = "Divorciado",
+                Selected = estadoCivil != null && estadoCivil == "Divorciado" ? true : false
+            });
+
+            ViewBag.Sexo = new MultiSelectList(lstSexo.ToList(), "Value", "Text");
+            ViewBag.EstadoCivil = new MultiSelectList(lstEstadoCivil.ToList(), "Value", "Text");
+        }
+
+        /** Método Referência Macoratti - https://www.macoratti.net/11/09/c_val1.htm **/
+        private bool ValidarCpf(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
         }
     }
 }
